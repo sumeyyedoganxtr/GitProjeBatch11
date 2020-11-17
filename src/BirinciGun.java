ce3041911cd5e54c156e60d8f59fed7fce685edf
sumeyyedoganxtr <74563027+sumeyyedoganxtr@users.noreply.github.com>
2020-11-17 22:14:23 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -4,6 +4,7 @@ public class BirinciGun {
 	public static  void main(String[] args) {
 		System.out.println("11:57  -  Birinci Gun");
 		System.out.println("ikinci Commit");
+		System.out.println("10:11 - İkinci Gün");
 		
 	}
 }
