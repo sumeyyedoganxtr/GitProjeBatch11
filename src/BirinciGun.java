eebfafd244a62076ecc7e7bf74e066d3251e5ed9
ihsan <sumeyyedoganxtr@gmail.com>
2020-11-17 21:06:57 +0300

    ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,7 +2,8 @@
 public class BirinciGun {
 
 	public static  void main(String[] args) {
-		System.out.println("11:05  -  Birinci Gun");
+		System.out.println("11:57  -  Birinci Gun");
+		System.out.println("ikinci Commit");
 		
 	}
 }
